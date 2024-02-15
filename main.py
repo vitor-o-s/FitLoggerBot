61f57b6e7ab3b3a03a0ca7cecc741e228a745ec5
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2024-02-15 17:12:49 -0300

    Formatação com blue

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from telegram import Update, ForceReply
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    CallbackContext,
+)
 import logging
 import os
 
 # Configuração do logging para facilitar a depuração
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
 
 logger = logging.getLogger(__name__)
 
 # Função para iniciar a conversa
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!"
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
     )
     """
     user = update.effective_user
@@ -23,20 +32,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     """
 
+
 # Função para registrar treinos
 def registrar(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Qual exercício você realizou?')
 
+
 # Função para responder uma mensagem em CAPS
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=text_caps
+    )
+
 
 # Adiciona um manipulador para mensagens de texto
 # def echo(update: Update, context: CallbackContext) -> None:
 #    update.message.reply_text(update.message.text)
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
 
 
 # Função principal para executar o bot
@@ -44,18 +60,20 @@ def main() -> None:
     # Carregar o token
     token = os.getenv('TELEGRAM_BOT_TOKEN')
     if not token:
-        logger.error("Token do Telegram não encontrado. Certifique-se de que a variável de ambiente TELEGRAM_BOT_TOKEN está definida.")
+        logger.error(
+            'Token do Telegram não encontrado. Certifique-se de que a variável de ambiente TELEGRAM_BOT_TOKEN está definida.'
+        )
         return
 
     # Inicializa o updater com o seu token do Bot
-    
+
     # updater = Updater(token)
 
     # Obtém o dispatcher para registrar manipuladores
     # dispatcher = updater.dispatcher
 
     application = ApplicationBuilder().token(token).build()
-    
+
     start_handler = CommandHandler('start', start)
     caps_handler = CommandHandler('caps', caps)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
@@ -63,14 +81,13 @@ def main() -> None:
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
     application.add_handler(start_handler)
-    
+
     # Registra os manipuladores de comandos e mensagens
     # application.add_handler(CommandHandler("start", start))
     # application.add_handler(CommandHandler("registrar", registrar))
     # application.add_handler(MessageHandler(filters.Text & ~filters.Command, echo))
 
     application.run_polling()
-
 
     # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
@@ -79,6 +96,7 @@ def main() -> None:
 
     # Bloqueia até que você pressione Ctrl+C ou o processo receba SIGINT, SIGTERM ou SIGABRT
     # updater.idle()
+
 
 if __name__ == '__main__':
     main()
