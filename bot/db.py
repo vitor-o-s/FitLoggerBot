25d7f9372bea875e048e6047562ec8a19cde343e
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2026-01-21 23:36:23 -0300

    Limpeza de código

--- bot/db.py
+++ bot/db.py
@@ -1,37 +1,23 @@
-from pymongo import MongoClient
 from datetime import datetime
+from os import getenv
+from pymongo import MongoClient
 
-CONNECTION_STRING = ''
+CONNECTION_STRING = getenv('MONGODB_CONNECTION_STRING', '')
 
 
-# def get_database():
-    
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    # db = MongoClient(CONNECTION_STRING)["workout_log"]
-
-    # Selecionar a coleção
-    # collection = db["workouts"]
-
-    # return collection
-
-def save_workout(exercise_name: str, reps: int, weight: float, load: float, user_id: int):
+def save_workout(
+    exercise_name: str, reps: int, weight: float, load: float, user_id: int
+):
     working_set = {
         'user': user_id,
         'exercise': exercise_name,
         'reps': reps,
         'weight': weight,
         'load': load,
-        'date': datetime.now()
+        'date': datetime.now(),
     }
-
-    collection = MongoClient(CONNECTION_STRING)["workout_log"]["workouts"]
+    if CONNECTION_STRING:
+        collection = MongoClient(CONNECTION_STRING)['workout_log']['workouts']
+    else:
+        collection = MongoClient()['workout_log']['workouts']
     return collection.insert_one(working_set)
-
-
-
-
-# This is added so that many files can reuse the function get_database()
-# if __name__ == '__main__':
-    
-    # Get the database
-    #dbname = get_database()
